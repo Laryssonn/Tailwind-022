9b77403ff3bb4949029b9e0dbe6885b94266648f
Larysson Nascimento <larysson_cb@hotmail.com>
2026-09-02 18:28:30 -0300

    feat: using tw-animate-css in the project

--- Vite/src/App.tsx
+++ Vite/src/App.tsx
@@ -1,6 +1,8 @@
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold text-emerald-500 hover:text-red-900 ">Hello Vite</h1>
+    <>
+      <h1 className="text-3xl font-bold text-emerald-500 hover:text-red-900 animate-in fade-in zoom-in duration-1600">Hello Vite</h1>
+    </>
   );
 };
